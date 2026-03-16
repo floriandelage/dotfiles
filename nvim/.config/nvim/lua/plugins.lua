31a56d916ca400867c1701518cffa1b68b768b9d
Florian Delage <floriandlg@icloud.com>
2026-03-16 23:59:28 +0100

    update nvim config

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -117,7 +117,7 @@ require("conform").setup({
 	format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
 	formatters_by_ft = {
 		lua = { "stylua" },
-		c = { "clang_format" },
+		c = { "clang-format" },
 	},
 })
 
